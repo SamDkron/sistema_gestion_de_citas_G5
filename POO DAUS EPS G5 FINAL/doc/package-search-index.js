1d2baf352e587f595fd31a88484c822b70abe09e
Samuel <samueldauf@gmail.com>
2025-11-09 18:25:09 -0500

    Eliminar carpeta que se duplico accidentalmente

--- POO DAUS EPS G5 FINAL/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"aplicación"},{"l":"controlador"},{"l":"data"},{"l":"modelo"},{"l":"service"},{"l":"vista"}];updateSearchResults();
